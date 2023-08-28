8145851e107ccd6d2bcc463b7c2f8ce01bd34cce
Elievelton <elievelton@gmail.com>
2023-08-28 17:40:52 -0300

    resolvido alguns problemas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1848a150ce864f21"; }
+/******/ 		__webpack_require__.h = function() { return "9c99f97c94def113"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
