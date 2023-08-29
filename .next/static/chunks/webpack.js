8f1d9fc898b11e1d03f7a7058f276b987a457947
elievelton <elievelton@gmail.com>
2023-08-29 15:49:26 -0300

    trabalhando logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bcd987346a953098"; }
+/******/ 		__webpack_require__.h = function() { return "2c6bde0c1b2b4a5c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.7f173ff65c5a6f32.hot-update.js
+++ .next/static/webpack/webpack.7f173ff65c5a6f32.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9d280121c6fe1e85"; }
+/******/ 	__webpack_require__.h = function() { return "2c6bde0c1b2b4a5c"; }
 /******/ }();
 /******/ 
 /******/ }
